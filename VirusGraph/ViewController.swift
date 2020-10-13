5ecee64e70b82865aa053ac3479434331069d9aa
Franklin Yang <fyang016@ucr.edu>
2020-10-13 12:51:02 -0700

    Initial Commit

---
+++ VirusGraph/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VirusGraph
+//
+//  Created by AdrenResi on 10/13/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
